92e7f0843822f84f55680a4a7831e95aa6694e40
melancholicpeoplearenotpeople <vondercold@mail.ru>
2025-12-23 14:29:00 +0300

    Add files via upload

---
+++ lesson1.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    val name = "Oleg"
+    var age: Int = 52
+    age += 10
+    println("Privet, ya $name, mne $age let")
+    println("Ti kto?")
+    var manName: String = readln()
+}
